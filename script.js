fca0af29168c2874c80d66a4446631a0f35149d5
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2024-10-20 22:38:26 +0200

    Validation Logic

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ function validateSyntax() {
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
+
+    //Expressing the format of a valid input field.
+    
+    let Check = /^pet_[a-zA-Z0-9]+$/;
+
+    //Statement to check the validator
+    if (Check.test(input)){
+        result = "Valid Syntax \u{1F7E2}";
+    }else {
+        result = "Invalid Syntax \ud83d\udd34";
+    }
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
             document.getElementById('result').innerText = result;
